af724e3ab088eb5792e5f6ce335a99ca86df10ee
NanSong52 <92899571+NanSong52@users.noreply.github.com>
2024-06-26 03:04:15 -0400

    Initial commit

--- main.cpp
+++ main.cpp
@@ -7,13 +7,8 @@ int main() {
 
 
     SkipList<int, std::string> skipList(6);
-	skipList.insert_element(1, "学"); 
-	skipList.insert_element(3, "算法"); 
-	skipList.insert_element(7, "认准"); 
-	skipList.insert_element(8, "微信公众号：代码随想录"); 
-	skipList.insert_element(9, "学习"); 
-	skipList.insert_element(19, "算法不迷路"); 
-	skipList.insert_element(19, "赶快关注吧你会发现详见很晚！"); 
+	skipList.insert_element(1, "234"); 
+
 
     std::cout << "skipList size:" << skipList.size() << std::endl;
 
